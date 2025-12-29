ca3938ea2770c370580a65079ef61f078f109187
samyakvaidya10 <samyakmcv@gmail.com>
2025-12-30 01:42:47 +0530

    code change

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyANK5EqUVDdDAupBMpYWotGRaJK-CJNbJw",
-  authDomain: "daily-planner-7e364.firebaseapp.com",
-  projectId: "daily-planner-7e364",
-  storageBucket: "daily-planner-7e364.firebasestorage.app",
-  messagingSenderId: "543166557016",
-  appId: "1:543166557016:web:9e6b184441654e7d87932d"
+apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
